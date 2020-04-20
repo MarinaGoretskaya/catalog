eb41b57427759689e7ad5454cce590ccd36a43d7
unknown <m.goretskaya@caffesta.com>
2020-04-21 00:42:01 +0300

    init project

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+// const bootstrap = require('bootstrap');
+const jquery = require('jquery');
+const watch = require('gulp-watch');
+
+const cssFiles = [
+	'./node_modules/bootstrap/dist/css/bootstrap.min.css',
+	'./scss/**/*.scss'
+]
+
+const jsFiles = [
+	'./node_modules/jquery/dist/jquery.min.js',
+	'./node_modules/bootstrap/dist/js/bootstrap.min.js',
+	// './node_modules/jquery.maskedinput/src/jquery.maskedinput.js',
+	// './node_modules/inputmask/dist/jquery.inputmask.min.js',
+	'./js/main.js'
+]
+
+function scripts() {
+	return gulp.src(jsFiles)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('./js'))
+}
+
+gulp.task('sass-compile', function () {
+	// return gulp.src('./scss/**/*.scss')
+	return gulp.src(cssFiles)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./css/'))
+})
+
+gulp.task('watch', function () {
+	gulp.watch('./scss/**/*.scss', gulp.series('sass-compile'))
+	gulp.watch('./js/**/*.js', scripts);
+})
+
+gulp.task('scripts', scripts);
+
---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+	let body = $(document);
+
+	// let countCow;
+	// let countMilk;
+	// let position;
+	// let contact;
+	// let contactValue;
+	//
+	// let resultData;
+	//
+	//
+	// let section1 = body.find('#section-rect-1');
+	//
+	//
+	// $('#section-rect-1').find('button').click(displaySection1);
+	//
+	// $('#section-rect-4').find('#sendData').click(submitData);
+
+
+});
+
