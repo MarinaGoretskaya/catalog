60b0ac6e5e44a6efaa1c8ff2c0165aeda88a4de3
unknown <m.goretskaya@caffesta.com>
2020-04-21 14:45:21 +0300

    edit markup

--- js/main.js
+++ js/main.js
@@ -1,22 +1,30 @@
 $(document).ready(function () {
 	let body = $(document);
+	let allNavItems = body.find('.nav-item');
+	let allSections = body.find('section');
 
-	// let countCow;
-	// let countMilk;
-	// let position;
-	// let contact;
-	// let contactValue;
-	//
-	// let resultData;
-	//
-	//
-	// let section1 = body.find('#section-rect-1');
-	//
-	//
-	// $('#section-rect-1').find('button').click(displaySection1);
-	//
-	// $('#section-rect-4').find('#sendData').click(submitData);
+	$('.nav-item').click(navigate);
 
+	function navigate() {
+		allNavItems.removeClass('active');
+		$(this).addClass('active');
+		displaySection($(this));
+	}
+
+	function displaySection(navItemActive) {
+		allSections.addClass('display-none');
+		if (navItemActive.hasClass('all')) {
+			$('.container.All').removeClass('display-none');
+		} else if (navItemActive.hasClass('bonfesto')) {
+			$('.container.Bonfesto').removeClass('display-none');
+		} else if (navItemActive.hasClass('villmonte')) {
+			$('.container.Villmonte').removeClass('display-none');
+		} else if (navItemActive.hasClass('cooking')) {
+			$('.container.Cooking').removeClass('display-none');
+		} else {
+			$('.container.all').removeClass('display-none');
+		}
+	}
 
 });
 
